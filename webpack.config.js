dc00aae16534de4a3143f253c9b55dac25a3111f
Yakirv <yakirv@gmail.com>
2025-03-02 21:18:32 +0200

    building the bases

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watchFile } = require('fs');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js', 
+
+  output: {
+    filename: 'main.js', 
+    path: path.resolve(__dirname, 'dist'), 
+    clean: true 
+  },
+
+  devtool:"eval-source-map",
+  devServer:{
+      watchFiles:["./src/template.html"],
+  },
+  stats: {
+    children: true,
+  },
+
+  mode: 'development', 
+
+  plugins: [new HtmlWebpackPlugin({ 
+    template: './src/template.html' })],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: "asset/resource",
+    }
+    ],
+  },
+};
